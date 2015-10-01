d93d7ee5298d6b8c78c9a5eb1083213bee9c871d
tbrams <torben@brams.dk>
2015-10-01 14:07:02 +0200

    Cancel search implemented

--- app/src/main/java/dk/incipio/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/dk/incipio/photogallery/PhotoGalleryFragment.java
@@ -2,8 +2,13 @@ package dk.incipio.photogallery;
 
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.ComponentName;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
@@ -19,6 +24,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
 
@@ -62,6 +68,24 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.fragment_photo_gallery, menu);
+
+        // Configure SearchView
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.HONEYCOMB) {
+            // Pull out the SearchView
+            MenuItem searchItem=menu.findItem(R.id.menu_item_search);
+            SearchView searchView = (SearchView) searchItem.getActionView();
+
+            // Get the date from our searchable.xml as SearchableInfo
+            SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+            ComponentName name = getActivity().getComponentName();
+            SearchableInfo searchInfo = searchManager.getSearchableInfo(name);
+
+            // For some reason this does not work - getActionView() above does not return
+            // anything usable for me, so I will have to check for null here :-(
+            if (searchView != null) {
+                searchView.setSearchableInfo(searchInfo);
+            }
+        }
     }
 
 
