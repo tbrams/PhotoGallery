509137ba6261d091fedc7e74cd90f3fb6493d9a1
tbrams <torben@brams.dk>
2015-10-02 10:44:24 +0200

    Rotation safe

--- app/src/main/java/dk/incipio/photogallery/PhotoPageFragment.java
+++ app/src/main/java/dk/incipio/photogallery/PhotoPageFragment.java
@@ -5,8 +5,11 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class PhotoPageFragment extends VisibleFragment {
     private String mUrl;
@@ -24,6 +27,13 @@ public class PhotoPageFragment extends VisibleFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_photo_page, container, false);
+
+        final ProgressBar progressBar = (ProgressBar) v.findViewById(R.id.progressBar);
+        progressBar.setMax(100);
+        final TextView titleTextview = (TextView) v.findViewById(R.id.titleTextView);
+
+
+
         mWebView = (WebView) v.findViewById(R.id.webView);
 
         mWebView.getSettings().setJavaScriptEnabled(true);
@@ -31,6 +41,21 @@ public class PhotoPageFragment extends VisibleFragment {
         mWebView.setWebViewClient(new WebViewClient() {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 return false;
+            }
+        });
+
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView webView, int progress) {
+                if (progress==100) {
+                    progressBar.setVisibility(View.INVISIBLE);
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress(progress);
+                }
+            }
+
+            public void onReceivedTitle(WebView webView, String title) {
+                titleTextview.setText(title);
             }
         });
 
